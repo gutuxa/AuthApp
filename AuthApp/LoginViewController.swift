0583a76b4b315c14a8bb3f368c66ea2fec7d6b13
Oksana Tugusheva <gutuxa@gmail.com>
2021-03-31 16:02:12 +0300

    HW_2.3 keyboard return actions

--- AuthApp/LoginViewController.swift
+++ AuthApp/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var usernameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
@@ -22,7 +22,10 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        usernameTF.delegate = self
+        passwordTF.delegate = self
+        
         loginButton.layer.cornerRadius = 5
     }
 
@@ -32,12 +35,12 @@ class LoginViewController: UIViewController {
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        if usernameTF.text == username && passwordTF.text == password {
-            return true
-        } else {
-            showAlert(message: "Wrong username or password")
-            return false
-        }
+        isFormValid()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     @IBAction func forgotButtonTouched(_ sender: UIButton) {
@@ -53,11 +56,33 @@ class LoginViewController: UIViewController {
         passwordTF.text = ""
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.usernameTF {
+            self.passwordTF.becomeFirstResponder()
+            return false
+        } else if isFormValid() {
+            performSegue(withIdentifier: "login", sender: loginButton)
+            return true
+        } else {
+            return false
+        }
+    }
+    
     private func showAlert(message: String) {
         let alert = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+    
+    private func isFormValid() -> Bool {
+        let isValid = usernameTF.text == username && passwordTF.text == password
+        
+        if !isValid {
+            showAlert(message: "Wrong username or password")
+        }
+        
+        return isValid
     }
     
 }
